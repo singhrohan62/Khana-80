40fcad1ae97baf18874fdfcb9442777702809984
Rohan Singh <rohaniiitv@gmail.com>
2018-06-25 00:17:13 +0530

    added react-slideshow-image module

--- src/Slider.js
+++ src/Slider.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
+import { Slide,Fade } from 'react-slideshow-image';
+
+	const imageArray = [
+        "https://images3.alphacoders.com/273/273289.jpg",
+        "https://images4.alphacoders.com/574/thumb-1920-574726.jpg",
+        "https://img00.deviantart.net/0093/i/2005/221/1/7/wobbuffet_wallpaper_by_xebeckle_il_ziluf.jpg",
+        "http://3.bp.blogspot.com/-PzgwkqKiT0c/VTvAK_eDQrI/AAAAAAAALuk/KkthF3qFt9A/s1600/pokemon-tumblr-wallpaper-6805925.jpg",
+        "http://www.qygjxz.com/data/out/252/4240257-wallpapers-pokemon.png"
+      ];
 
 export default class Slider extends Component {
 	
-	componentWillMount() {
+
+	/*componentWillMount() {
 	let imageArray = [
         "https://images3.alphacoders.com/273/273289.jpg",
         "https://images4.alphacoders.com/574/thumb-1920-574726.jpg",
@@ -22,11 +32,13 @@ export default class Slider extends Component {
       setTimeout(refreshingBackground, Duration);
     }
     refreshingBackground();
-	}
+	}*/
 
 	render () {
 		return (
-			<p></p>
+			<Slide images = {imageArray}
+			duration = {3000}
+			transitionDuration = {1000}/>
 			);
 	}
 }
