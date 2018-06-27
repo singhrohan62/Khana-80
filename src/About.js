d2606b2a80c7ef8ce17a891df73a2a826053dc2a
Rohan Singh <rohaniiitv@gmail.com>
2018-06-28 00:23:32 +0530

    completed about component

--- src/About.js
+++ src/About.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import './About.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+    root : ({
+         ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 10,
+        paddingBottom: theme.spacing.unit * 10,
+        backgroundColor : "transparent",
+         marginTop: theme.spacing.unit * 8,
+         marginLeft: theme.spacing.unit * 2,
+    }),
+});
 
 class About extends React.Component {
 
     render () {
+        const {classes} = this.props;
         return(
             <div>
-                    <p class="typewriter">N I R V A N A</p>
-                    
-                    <h3> In the heart of Varanasi sits Café Nirvana, almost like a tree house.
-                     Sitting on its terrace watching one of the most spectacular sunsets,
-                      you can almost forget that you are actually in a busy Varanasi marketplace.
-                    </h3>
+                  
+                    <div className="paper-component">
+                        <Paper className = {classes.root} elevation = {8}>
+                              <p class="typewriter">N I R V A N A</p>
+                            <h6>
+                                Café Niravana, Varanasi’s most charming café is situated at the edge of river Ganga, in Assi Ghat. 
+                                The idea was to create a peaceful and lively space where people could have a wonderful meal, 
+                                relax and feel rejuvenated, meet over aromatic coffees and fresh juices accompanied with delicious cakes,
+                                exchange ideas over wholesome global cuisine and ‘just be’ in a friendly and aesthetic environment with 
+                                friends and family enjoying nature’s bounty and witnessing the beauty of the city.
+                            </h6>
+                            
+                        </Paper>
+                    </div>
             </div>
         );
     }
 }
+About.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default About;
+export default withStyles(styles)(About);
