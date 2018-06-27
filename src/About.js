e1267222ca296f341e8979f89527c74bbbb856e8
Rohan Singh <rohaniiitv@gmail.com>
2018-06-27 22:43:56 +0530

    added material-ui

--- src/About.js
+++ src/About.js
@@ -6,7 +6,12 @@ class About extends React.Component {
     render () {
         return(
             <div>
-                    <p class="typewriter">NIRVANA</p>
+                    <p class="typewriter">N I R V A N A</p>
+                    
+                    <h3> In the heart of Varanasi sits Café Nirvana, almost like a tree house.
+                     Sitting on its terrace watching one of the most spectacular sunsets,
+                      you can almost forget that you are actually in a busy Varanasi marketplace.
+                    </h3>
             </div>
         );
     }
