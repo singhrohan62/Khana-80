56008e93ec036fac48fa8c30aeb14eb4b140828a
Rohan Singh <rohaniiitv@gmail.com>
2018-06-30 16:54:53 +0530

    added contacting component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Slider from './Slider';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import About from "./About";
 import Menu from "./Menu";
+import Contact_Us from "./Contact_Us";
 
 class App extends Component {
   render() {
@@ -20,14 +21,16 @@ class App extends Component {
                   <li className="route active"><Link to="/">Home</Link></li>
                   <li className="route"><Link to="/about">about</Link></li>
                   <li className="route"><Link to="/menu">Menu</Link></li>
+                  <li className="route"><Link to="/Contact_Us">Contact us </Link></li>
               </ul>
               </nav>
               <Route exact path="/" component={Slider}/>
               <Route path="/about" component={About}/>
               <Route path="/menu" component={Menu}/>
+              <Route path="/Contact_Us" component={Contact_Us}/>
               </div>
               </Router>
-    
+
       </div>
     );
   }
---
+++ src/components/Contact_Us.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Contact_Us extends Component {
+	render () {
+		return (
+			<p>Contact_Us</p>
+			);
+	}
+}
+
+export default Contact_Us;
