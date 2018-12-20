60d9351cbe82ca1642c2613d310395849b570b4c
Rohan Singh <rohaniiitv@gmail.com>
2018-12-20 20:21:08 +0530

    added link to about component and removed redundant component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Typist from 'react-typist'
+import Typist from 'react-typist';
+import { Link } from 'react-router-dom'
 import '../css/App.css';
+import 'bulma/css/bulma.css'
 
-import picture from '../static/picture_1.jpg'
 
 class App extends Component {
   render() {
@@ -13,12 +14,12 @@ class App extends Component {
                   <Typist>
                   <h1 className="home-header">Cafe Nirvana</h1>
                    <Typist.Delay ms={500} />
-                    <h2>
+                    <h2 className="home-subheader">
                       Awesome place to chill
                       <Typist.Backspace count={8} delay={200}/><span>for foodies</span>
                       <Typist.Backspace count={11} delay={200}/><span> to hangout with friends</span>
-                      <br/>
-                      <Typist.Delay ms={500}/><button>Help</button>
+                      <br/><br/>
+                      <Typist.Delay ms={500}/><Link to="/about" className="button is-link is-rounded" >Check In</Link>
                     </h2>
                   </Typist>
               </div>
